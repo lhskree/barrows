b9251274c597fc6dade4cfc22b03d62ee8094b87
unknown <logan@DRAU-THINK.cleverex.com>
2015-05-05 17:27:37 -0400

    form validation works for creating a user - now to login and set cookies

--- auth.js
+++ auth.js
@@ -2,9 +2,73 @@ $(document).ready(function () {
 
 	$.post("./init.php")
 	.success(function (data) {
+
+		// If there's no user data, show the create authoring account form
 		if (!data.userExists) {
+
+			// Add the form to the outlet
 			$("#outlet").append(data.form);
+
+			$("#usernameError").hide();
+			$("#passwordError1").hide();
+			$("#passwordError2").hide();
+
+			// Register submit handler
+			$("#submitCreateUser").click(validateCreateAuth);
+		} else {
+
+			// Show the login form
+
+			// Add the form to the outlet
+			$("#outlet").append(data.form);
+
 		}
+	})
+	.fail(function () {
+		console.log("Failed.");
 	});
 
 });
+
+function validateCreateAuth() {
+	
+	var valid = true;
+
+	var u = $("#username").val();
+	
+	if (u.length < 8) {
+		$("#usernameError").show();
+		valid = false;
+	} else {
+		$("#usernameError").hide();
+	}
+
+	var p1 = $("#pass1").val(),
+		p2 = $("#pass2").val();
+
+	if (p1.length < 16 || p2.length < 16) {
+		$("#passwordError1").show();
+		valid = false;
+	} else {
+		$("#passwordError1").hide();
+	}
+
+	if (p1 !== p2) {
+		$("#passwordError2").show();
+		valid = false;
+	} else {
+		$("#passwordError2").hide();
+	}
+
+	if (valid) {
+		console.log("Validated. Attempting post request . . .");
+		$.post("./init.php", $("#createUser").serialize())
+		.success(function (data) {
+			console.log(data);
+		})
+		.fail(function () {
+			console.log("Failed to connect to init.php");
+		});
+	}
+
+}
--- init.php
+++ init.php
@@ -21,29 +21,88 @@ if (!$result) {
 		. "pass VARCHAR(50), "
 		. "PRIMARY KEY (pass))";
 
-	if ($mysqli->query($createSettings) === TRUE) {
+	if ($mysqli->query($createSettings) === true) {
 		$response["Created Settings"] = "Successfully created 'settings'.";
 	} else {
 		$response["Created Settings"] = "Invalid query : " . $mysqli->error;
 	}
 }
 
-// Check if a user has been created
-$result = $mysqli->query("SELECT * FROM settings");
+// If there's a request to make a user account
+if ($_POST) {
+	if (isset($_POST["createUser"])) {
 
-if (!$result->num_rows) {
-	header("Content-Type: application/json");
-	echo json_encode(["userExists" => false,
-		"form" => "<h1>Create an Authoring Account</h1>"
-		. "<form id='createUser'>"
-		. "<label for='username'>Username</label>"
-		. "<input type='text' id='username' name='username'>"
-		. "<label for='pass1'>Password</label>"
-		. "<input type='password' id='pass1' name='pass1'>"
-		. "<label for='pass2'>Password (Again)</label>"
-		. "<input type='password' id='pass2' name='pass2'>"
-		. "</form>"]);
-	exit;
+		$u = $mysqli->real_escape_string($_POST["username"]);
+		$p1 = $mysqli->real_escape_string($_POST["pass1"]);
+		$p2 = $mysqli->real_escape_string($_POST["pass2"]);
+
+		if ($u !== $_POST["username"]
+			|| $p1 !== $_POST["pass1"]
+			|| $p2 !== $_POST["pass2"]
+			|| $p1 !== $p2) {
+
+			$response["validationError"] = true;
+			header("Content-Type: application/json");
+			echo json_encode($response);
+			exit;
+		} else {
+
+			$query = "INSERT INTO settings (author, pass) VALUES('" . $u . "', '" . $p1 . "')";
+
+			if ($mysqli->query($query) === true) {
+
+				$response["accountCreated"] = true;
+				header("Content-Type: application/json");
+				header("Cookie: loggedin=true");
+				echo json_encode($response);
+				exit;
+			} else {
+				$response["insertError"] = "Invalid query : " . $mysqli->error;
+				header("Content-Type: application/json");
+				echo json_encode($response);
+				exit;
+			}
+		}
+		
+	}
+} else {
+	// Check if a user has been created
+	$result = $mysqli->query("SELECT * FROM settings");
+
+	// There's no user
+	if (!$result->num_rows) {
+		header("Content-Type: application/json");
+		echo json_encode(["userExists" => false,
+			"form" => "<h1>Create an Authoring Account</h1>"
+			. "<form id='createUser'>"
+			. "<label for='username'>Username</label>"
+			. "<input type='text' id='username' name='username' required>"
+			. "<span id='usernameError'>Username must be at least 8 characters</span>"
+			. "<label for='pass1'>Password</label>"
+			. "<input type='password' id='pass1' name='pass1' required>"
+			. "<span id='passwordError1'>Passwords must be 16 characters</span>"
+			. "<label for='pass2'>Password (Again)</label>"
+			. "<input type='password' id='pass2' name='pass2' required>"
+			. "<span id='passwordError2'>Passwords must match</span>"
+			. "<input type='hidden' name='createUser' value='true'>"
+			. "</form>"
+			. "<button id='submitCreateUser'>Submit</button>"]);
+		exit;
+	} else {
+		// There is a user
+		header("Content-Type: application/json");
+		echo json_encode(["userExists" => true,
+			"form" => "<h1>Login to Authoring Account</h1>"
+			. "<label for='username'>Username</label>"
+			. "<input type='text' id='username' name='username' required>"
+			. "<span id='usernameError'>Username must be at least 8 characters</span>"
+			. "<label for='pass1'>Password</label>"
+			. "<input type='password' id='pass1' name='pass1' required>"
+			. "<span id='passwordError1'>Passwords must be 16 characters</span>"
+			. "</form>"
+			. "<button id='login'>Login</button>"]);
+		exit;
+	}
 }
 
 ?>
