4f618615cdb4de43c295688a00dca85a1ec39d58
unknown <logan@DRAU-THINK.cleverex.com>
2015-05-12 15:52:39 -0400

    unlock works, yay

--- js/barrows.js
+++ js/barrows.js
@@ -43,6 +43,34 @@ function renderRecentPosts (posts) {
 
 		var template = _.template($("#postSnippet").html());
 		$pl.append(template(post));
+		bindClickHandler(post.id);
+
+	});
+
+}
+
+function bindClickHandler(id) {
+
+	$("#postUnlock-" + id).click(function () {
+
+		var key = "#postKey-" + id;
+		var key = escape($(key).val());
+
+		var data = {"req":"unlock","key":key,"id":id};
+		console.log(data);
+
+		$.post("./php/unlock.php", data)
+		.success(function (data) {
+
+			console.log(data);
+			$("#post-" + data.id + " .postBodyContent").text(data.body);
+
+		})
+		.fail(function () {
+
+			console.log("Something went wrong while decrypting. . .");
+
+		});
 
 	});
 
